05be78120dd3c75760443e10ca61b806b85879e6
RahmadinaIkaP <rahmadina.ika@student.umn.ac.id>
2021-10-25 20:04:23 +0700

    Tugas Lab Week 8

---
+++ RahmadinaIkaPutri_00000034690_IF570_EL_UTS/app/src/main/java/id/ac/umn/rahmadinaikaputri_00000034690_if570_el_uts/LoginActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.rahmadinaikaputri_00000034690_if570_el_uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
