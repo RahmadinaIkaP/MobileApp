0f0de9670df0563ba8c468fdd024ad83ad2061c1
RahmadinaIkaP <rahmadina.ika@student.umn.ac.id>
2021-11-19 11:28:14 +0700

    Tugas Lab Week 12

---
+++ MyIntentApp/app/src/main/java/id/ac/umn/myintentapp/MoveWithDataActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoveWithDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+    }
+}
